7d4a28674a51337a9c723ff602c15e67110b5518
Jan Czok <jan.czok@outlook.de>
2021-06-15 11:04:51 +0200

    added axios request to NASA's endpoint APOD

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -1,3 +1,20 @@
-exports.getAPODImage = function(req, res, next) {
+const axios = require('axios');
+
+exports.getAPODImage = function (req, res, next) {
+    // CREATE THE API CALL TO THE NASA MIDDLEWARE APOD
+    // Make a request for a user with a given ID
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=MGnq13mROoczzza0IxyX3MhxN1BFetcNPbp6upTg')
+        .then(function (response) {
+            // handle success
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+
     res.send('NOT IMPLEMENTED');
 };
